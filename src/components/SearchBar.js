6942e2f8c3cea78101c7c69bd8ca99348d2b0c0b
annu <annu.pawaria@gmail.com>
2024-06-07 15:31:31 +0530

    completed navbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  {SearchIcon} from "@mui/icons-material"
+import {Paper,IconButton} from '@mui/material'
+
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <Paper>SearchBar</Paper>
   )
 }
 
