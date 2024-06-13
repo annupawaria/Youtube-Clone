1ddb40a6fb65c8e2908942dd67a7c9255e4727cd
annu <annu.pawaria@gmail.com>
2024-06-13 14:38:55 +0530

    adding videodetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import{Link,useParams} from 'react-router-dom'
+import {Typography,Box,Stack} from '@mui/material';
+import {CheckCircle} from "@mui/material"
+import {Video} from './'
+import{fetchFromAPI} from '../utils/fetchFromAPI'
+import ReactPlayer from 'react-player';
 
 const VideoDetail = () => {
+  const[VideoDetail,setVideoDetail]=useState(null)
+  const{id} =useParams();
+  useEffect(()=>{
+    fetchFromAPI(`video?part=snippet,statistics&id=${id}`).then((data)=>setVideoDetail(data.items[0]));
+  },[id])
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+<Stack direction={{xs:'column',md:"row"}}>
+  <Box flex={1}>
+    <Box sx={{width:'100%',position:'sticky'}}>
+    <ReactPlayer url={`https://www.youtube.com/wach?v=${id}`}/>
+    </Box>
+  </Box>
+
+
+
+
+</Stack>
+
+
+
+    </Box>
   )
 }
 
