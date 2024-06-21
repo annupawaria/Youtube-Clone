ed92a49b794a8c39e3825eb07a6a082c3f951bf5
annu <annu.pawaria@gmail.com>
2024-06-21 14:08:57 +0530

    adding channel card content

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Box,CardContent,CardMedia,Typography} from "@mui/material"
+import {CheckCircle} from '@mui/icons-material'
+import {Link} from 'react-router-dom'
+import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = () => {
+const ChannelCard = ({channelDetail}) => {
   return (
-    <div>ChannelCard</div>
+    <Box sx={{boxShadow:'none',borderRadius:'20px'}}>
+<Link to={`/channel/${channelDetail?.id?.channelId}`}>
+<CardContent sx={{display:"flex",flexDirection:"column",justifyContent:'center',textAlign:"center",color:"grey"}}>
+
+</CardContent>
+</Link>
+
+
+    </Box>
   )
 }
 
