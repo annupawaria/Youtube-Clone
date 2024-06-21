17a685bb3b105fe28642f6eb0ba97bf42eb03773
annu <annu.pawaria@gmail.com>
2024-06-21 17:15:13 +0530

    facing some issues in videocard component

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -6,12 +6,8 @@ import { demoProfilePicture } from '../utils/constants'
 
 const ChannelCard = ({channelDetail}) => {
   return (
-    <Box sx={{boxShadow:'none',borderRadius:'20px'}}>
-<Link to={`/channel/${channelDetail?.id?.channelId}`}>
-<CardContent sx={{display:"flex",flexDirection:"column",justifyContent:'center',textAlign:"center",color:"grey"}}>
+    <Box >
 
-</CardContent>
-</Link>
 
 
     </Box>
--- src/components/Videocard.js
+++ src/components/Videocard.js
@@ -5,34 +5,13 @@ import { CheckCircle } from "@mui/icons-material"
 import { demoChannelTitle, demoThumbnailUrl, demoVideoTitle, demoChannelUrl, demoVideoUrl } from '../utils/constants'
 
 
-const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+const VideoCard = ({video:{id:{videoId},snippet}}) => {
   return (
-    <Card sx={{width:{md:'320',xs:"100%"},boxShadow:"none",borderRadius:0}}>
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <CardMedia image={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
-
-        />
-
-      </Link>
-      <CardContent sx={{ background: "grey", height: '106px' }}>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant='subtitle1' fontWeight="bold" color="white">
-            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-
-          </Typography>
-
-        </Link>
-        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}`:demoChannelUrl}>
-          <Typography variant='subtitle2' fontWeight="bold" color="lightgrey">
-            {snippet?.channelTitle || demoChannelTitle}
-<CheckCircle sx={{fontSize:"12px", color:'lightgray', ml:3 }}/>
-          </Typography>
-
-        </Link>
-
-      </CardContent>
+    <Card>
+          <Link to={videoId ?`/video/${videoId}`:demoVideoUrl}>
+          <CardMedia image={snippet?.thumbnails?.high?.url}/>
+          </Link>
+          
     </Card>
   )
 }
