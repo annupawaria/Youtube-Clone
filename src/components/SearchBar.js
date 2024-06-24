b7a7ae8c11568d895028f775f08d1711b20a228e
annu <annu.pawaria@gmail.com>
2024-06-24 15:16:27 +0530

    first commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Stack,Box,IconButton} from "@mui/material" 
 import {Search} from "@mui/icons-material"
+import { Navigate, useNavigate } from 'react-router-dom'
 
 
 
 const SearchBar = () => {
+const[searchTerm,setSearchTerm]=useState("")
+const Navigate=useNavigate();
+const handleSubmit=(e)=>{
+  e.preventDefault(e);
+  if(searchTerm){
+    Navigate(`/search/${searchTerm}`)
+    setSearchTerm("")
+  }
+}
   return (
-    <Stack>
+    <Stack onSubmit={handleSubmit}>
 <input className='search-bar'
 placeholder='Search.....'
-value="" style={{height:'42px',borderRadius:'20px',
+value={searchTerm} style={{height:'42px',borderRadius:'20px',
   border:'1px solid red'
-}}
+}
+}
+onChange={(e)=setSearchTerm(e.target.value)}
 />
 <IconButton>
   <Search/>
