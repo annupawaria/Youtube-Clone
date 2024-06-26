bed7ea27911e53fccc426678c725ca20703d5502
annu <annu.pawaria@gmail.com>
2024-06-26 16:08:58 +0530

    completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Stack,Box} from "@mui/material"
+import { logo } from '../utils/constants'
+import {Link} from "react-router-dom"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box direction="row" sx={{height:"20vh",display:'flex',flexDirection:"row",justifyItems:"cenetr",alignItems:'center',justifyContent:'space-between',position:"sticky"}}>
+<Link to="/">
+  <img src={logo} style={{height:"66px",width:"66px"}}/>
+</Link>
+<Box>
+<input type="text" placeholder="serach......" style={{borderRadius:"44px",height:"44px",width:"366px",fontSize:"22px",justifyItems:"cenetr",alignItems:'center',}}/>
+
+</Box>
+
+    </Box>
   )
 }
 
