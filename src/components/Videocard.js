38e95923cccee816f3b7ecdb219522fee336998d
annu <annu.pawaria@gmail.com>
2024-07-02 13:06:26 +0530

    started a new component

--- src/components/Videocard.js
+++ src/components/Videocard.js
@@ -7,15 +7,32 @@ import { Link } from 'react-router-dom'
 function VideoCard({video:{id:{videoId},snippet}}) {
   
   return (
-    <Card>
+    <Card sx={{width:{md:'320px',xs:"100%"}}}>
       <Link to={videoId ? `/video/${videoId}`:demoVideoUrl} >
     <CardMedia image={snippet?.thumbnails?.high?.url}
     alt={snippet?.title}
+
     sx={{width:358,height:180}}
     
     />
     
     </Link>
+    <CardContent sx={{background:"grey",height:"106px"}}>
+      <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
+      <Typography variant="subtitle1" sx={{color:"lightgray" ,fontWeight:"bold"}}>
+{snippet?.title.slice(0,60)||
+demoVideoTitle(0,60)}
+      </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}`:demoChannelUrl}>
+      <Typography variant="subtitle2"  sx={{color:"lightgray" ,fontWeight:"bold"}}>
+        {snippet?.channelTitle || demoChannelTitle}
+        <CheckCircle sx={{ color:"lightgray", fontSize:"18px",ml:2,mt:1}}/>
+      </Typography>
+      
+      </Link>
+      
+    </CardContent>
     </Card>
   )
 }
