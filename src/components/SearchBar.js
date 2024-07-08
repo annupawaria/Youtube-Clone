26e2fa61b0c71871d8c8eeafca1c0b86e206f162
annu <annupawadia@gmail.com>
2024-07-08 15:56:09 +0530

    some changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,45 @@
-import React, { useState } from 'react'
-import { Box, IconButton } from "@mui/material"
-import Search from "@mui/icons-material/Search"
-import { useNavigate } from 'react-router-dom'
-const SearchBar = () => {
-  const[searchTerm,setSearchTerm]=useState("")
-  const navigate=useNavigate()
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
-const handleSubmit=(e)=>{
-e.preventDefault()
-if(searchTerm){
-  navigate(`/search/${searchTerm}`);
-  setSearchTerm("")
-}
-}
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Box sx={{
-          display: 'flex',
-          alignItems: 'center',
-          width: 'fit-content',
-          mr:{sm:5}
-        }}>
- <input type="Text" placeholder="search......"  style={{height:"33px",width:"300px",borderRadius:"22px",fontWeight:"bold",fontSize:"15px",border:"none"}}  className='search-bar' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
- <IconButton type="submit" sx={{p:"10px",color:"red"}}>
-  <Search/>
- </IconButton>
- </Box>
-    </form>
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
 
-  )
-}
-
-export default SearchBar
+export default SearchBar;
